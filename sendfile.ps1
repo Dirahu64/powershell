e33b14d6cded07fdd35aa8c05c7166b62249ad8c
Dirahu64 <Dirahu64@users.noreply.github.com>
2014-09-27 21:57:03 +0200

    debut init file pour send

--- sendfile.ps1
+++ sendfile.ps1
@@ -1,3 +1,6 @@
 # liste les pdf pour init fichier
 write-output "fichier" >tmp_file_list.csv 
 Get-ChildItem *.pdf -name >>tmp_file_list.csv 
+
+# init fichier pour envoi
+write-output "subject,file" >file_list.csv
