43fe74cd90ca36f176805582f839b1fb2a1e0c4d
Dirahu64 <Dirahu64@users.noreply.github.com>
2014-09-27 21:58:55 +0200

    fin des creations de liste pour envoi

--- sendfile.ps1
+++ sendfile.ps1
@@ -4,3 +4,8 @@ Get-ChildItem *.pdf -name >>tmp_file_list.csv
 
 # init fichier pour envoi
 write-output "subject,file" >file_list.csv
+$list = Import-Csv tmp_file_list.csv 
+foreach ($entry in $list)
+{
+write-output "$($entry.fichier),$($entry.fichier)" >>file_list.csv
+}
